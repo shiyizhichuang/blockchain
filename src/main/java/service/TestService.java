5847e9d8ae8a591d74e068da884d31f0da3f7d18
15527370365 <76:88:04:09:d1:e9:0f:f4:7b:de:30:99:c3:e0:6c:e5>
2017-03-23 16:16:22 +0800

    lvtao

---
+++ src/main/java/service/TestService.java
@@ -0,0 +1,8 @@
+package service;
+
+/**
+ * Created by mac on 2017/3/22.
+ */
+public interface TestService{
+    void test(String userid);
+}
---
+++ src/main/java/service/implement/TestServiceImpl.java
@@ -0,0 +1,14 @@
+package service.implement;
+
+import service.TestService;
+
+/**
+ * Created by mac on 2017/3/22.
+ */
+public class TestServiceImpl implements TestService {
+
+    @Override
+    public void test(String userid) {
+
+    }
+}
